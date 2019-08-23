add7090dfe0b3041e6c1b740901e0079cac968d2
MarvinChung <you@example.com>
2019-08-23 20:57:00 +0800

    shuffle objective for pretraining

--- BIG-BIRD/dataset.py
+++ BIG-BIRD/dataset.py
@@ -3,6 +3,7 @@ from torch.utils import data
 import numpy as np
 import json
 from tqdm import tqdm_notebook as tqdm
+import random
 #from tqdm import tqdm
 
 class Dataset(data.Dataset):    
@@ -38,53 +39,48 @@ class Dataset(data.Dataset):
         return self.size
     def __getitem__(self, index):
         return torch.from_numpy(self.documents[index]), torch.tensor([self.scores[index]]), torch.from_numpy(self.summaries[index])
+
+class PretrainDataset(data.Dataset):    
+    def __init__(self, data_name, INPUT_MAX, OUTPUT_MAX, pad_idx, cutoff=None):
+        print("loading json")
+        data = json.load(open(data_name, 'r'))
+        print("load json done.")
+        data_list = data['text']
+        
+        if cutoff is not None:
+            data_list = data_list[:cutoff]
+        # idata -> list
+        self.size = len(data_list)
+        
+        self.documents = np.full((self.size, INPUT_MAX), pad_idx, dtype=np.int64)
+        self.summaries = np.full((self.size, INPUT_MAX), pad_idx, dtype=np.int64)
+        
+        for i in tqdm(range(self.size)):
+            src = list(data_list[i])
+            tgt = data_list[i]
+            random.shuffle(src)
+            cp_src = min(len(src), INPUT_MAX)
+            cp_tgt = min(len(tgt), INPUT_MAX)
+            
+            self.documents[i,:cp_src] = src[:cp_src]
+            self.summaries[i,:cp_tgt] = tgt[:cp_tgt]
+        
+        print(self[0])
+        
+     
+    def __len__(self):
+        return self.size
+    def __getitem__(self, index):
+        return torch.from_numpy(self.documents[index]), torch.from_numpy(self.summaries[index])
     
-def make_data_generator(data_name, in_max, out_max, padding_idx, batch_size, cutoff=None, shuffle=True, num_workers=4):    
-    data_set = Dataset(data_name, in_max, out_max, padding_idx, cutoff)
+def make_data_generator(data_name, in_max, out_max, padding_idx, batch_size, pretrain=False, cutoff=None, shuffle=True, num_workers=4):    
+    if pretrain:
+        data_set = PretrainDataset(data_name, in_max, out_max, padding_idx, cutoff)
+    else:
+        data_set = Dataset(data_name, in_max, out_max, padding_idx, cutoff)
+    
     params = {'batch_size':batch_size,
          'shuffle': shuffle,
          'num_workers': num_workers}
     generator = data.DataLoader(data_set, **params)
     return data_set, generator
-
-class DynamicDataset(data.Dataset):   
-    def __init__(self, data_name, INPUT_MAX, OUTPUT_MAX, pad_idx, cutoff=None):
-        super().__init__()
-        
-        print("loading json")
-        data = json.load(open(data_name, 'r'))
-        print("load json done.")
-        sum_list = data['summary']
-        data_list = data['text']
-        score_list = data['score']
-        
-        if cutoff is not None:
-            sum_list = sum_list[:cutoff]
-            data_list = data_list[:cutoff]
-            score_list = score_list[:cutoff]
-            
-        # idata -> list
-        self.size = len(sum_list)
-        
-        batching_order = sorted(range(self.size), key=lambda i: len(data_list[i])) # ascending in length
-        sum_list = [sum_list[i] for i in batching_order]
-        data_list = [data_list[i] for i in batching_order]
-        score_list = [score_list[i] for i in batching_order]
-        
-        
-#         self.scores = np.asarray(, dtype=np.float64)  
-        
-#         self.documents = np.full((self.size, INPUT_MAX), pad_idx, dtype=np.int64)
-#         self.summaries = np.full((self.size, OUTPUT_MAX), pad_idx, dtype=np.int64)
-        
-#         for i in tqdm(range(self.size)):
-#             src = data_list[i]
-#             tgt = sum_list[i]
-#             cp_src = min(len(src), INPUT_MAX)
-#             cp_tgt = min(len(tgt), OUTPUT_MAX)
-            
-#             self.documents[i,:cp_src] = src[:cp_src]
-#             self.summaries[i,:cp_tgt] = tgt[:cp_tgt]
-        
-#         self.start = 0
-#         self.end = self.size
